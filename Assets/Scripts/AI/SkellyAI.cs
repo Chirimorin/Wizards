b76080faf0ce24368cb23b895503d490c13391df
Thomas Voogt <chirimorin@gmail.com>
2014-05-17 20:44:18 +0200

    skelly AI start

---
+++ Assets/Scripts/AI/SkellyAI.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkellyAI : MonoBehaviour {
+
+	//horizontal handling
+	public float speedx;
+	public float speedy;
+	public float acceleration;
+	public float brakeSpeed;
+	
+	public float targetSpeed;
+	private float axis;
+	private Vector3 vec;
+	
+	//Jumping
+	public int jumpheight;
+	private bool isGrounded;
+	private bool isGrounded2;
+
+	public GameObject parent; 
+
+	// Use this for initialization
+	void Start () {
+		parent = GameObject.Find ("NecroFT(Clone)");
+		acceleration = 0.67f;
+		targetSpeed = 3.5f;
+		brakeSpeed = 1.00f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float distance = parent.transform.position.x - transform.position.x;
+
+		if (distance < -1.5) {
+			xAxisMovement (-1);
+		}
+		else if (distance > 1.5) {
+			xAxisMovement (1);
+		}
+
+
+
+		Debug.Log (distance);
+	}
+
+	void xAxisMovement(int axis){
+		transform.position += vec;
+		vec = new Vector3 (speedx * Time.deltaTime, speedy, 0);
+		
+		if (axis == -1) {
+			speedx -= acceleration;
+			Vector3 rotate = transform.localScale;
+			rotate.x = -0.5f;
+			transform.localScale = rotate;
+			if (speedx < targetSpeed * -1) {
+				speedx = -1 * targetSpeed;
+				speedx += 0;
+			}
+			
+		} else if (axis == 1) {
+			speedx += acceleration;
+			Vector3 rotate2 = transform.localScale;
+			rotate2.x = 0.5f;
+			transform.localScale = rotate2;
+			if(speedx > targetSpeed){
+				speedx = targetSpeed;
+				speedx += 0;
+			}
+			
+		} else if (axis == 0) {
+			if (speedx <= targetSpeed && speedx > 0) {
+				speedx -= brakeSpeed;
+				if (speedx <= 0) {
+					speedx = 0;
+				}
+			}
+			if (speedx >= -targetSpeed && speedx < 0) {
+				speedx += brakeSpeed;
+				if (speedx >= 0) {
+					speedx = 0;
+				}
+			}
+		}
+	}
+	
+}
--- Assets/Scripts/Characterbound/SummonSK.cs
+++ Assets/Scripts/Characterbound/SummonSK.cs
@@ -7,17 +7,13 @@ public class SummonSK : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
-
-
 		for (int i = 0; i < 1; i++) {
 			Instantiate (skeleton, new Vector3 (0, 0, 0), Quaternion.Euler (0, 0, 0));
-			
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log ((skeleton.transform.position.x-transform.position.x));
+		//Debug.Log ((skeleton.transform.position.x-transform.position.x));
 	}
 }
