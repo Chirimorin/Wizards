66cbb4fa7fa52803e5714a6abd2d0fc785ec880b
Jorentjuh <jorentjuh@hotmail.com>
2014-05-17 20:46:03 +0200

    Extra shit met platformen

--- Assets/Scripts/Characterbound/SummonSK.cs
+++ Assets/Scripts/Characterbound/SummonSK.cs
@@ -7,9 +7,6 @@ public class SummonSK : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
-
-
 		for (int i = 0; i < 1; i++) {
 			Instantiate (skeleton, new Vector3 (0, 0, 0), Quaternion.Euler (0, 0, 0));
 			
@@ -18,6 +15,6 @@ public class SummonSK : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log ((skeleton.transform.position.x-transform.position.x));
+		//Debug.Log ((skeleton.transform.position.x-transform.position.x));
 	}
 }
---
+++ Assets/Scripts/StageScripts/Platforming.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Platforming : MonoBehaviour {
+
+	private GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.Find ("NecroFT(Clone)");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (Player.transform.position);
+	}
+}
