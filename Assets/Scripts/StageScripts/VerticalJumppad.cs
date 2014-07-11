05e9a44b911bd4cfac5f8e38fc6f1f47104866d6
Thomas Voogt <chirimorin@gmail.com>
2014-07-11 13:38:10 +0200

    Jumppad improvement

--- Assets/Scripts/StageScripts/VerticalJumppad.cs
+++ Assets/Scripts/StageScripts/VerticalJumppad.cs
@@ -3,8 +3,7 @@ using System.Collections;
 
 public class VerticalJumppad : MonoBehaviour {
 
-	public float power = 1600;
-	private GameObject player;
+	public float power = 1000;
 
 	void Start(){
 
@@ -12,17 +11,12 @@ public class VerticalJumppad : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		try{
-			player = GameObject.Find ("NecroFT(Clone)");
-		}catch(UnityException e){
 
-		}
 	}
 
 	void OnCollisionEnter2D(Collision2D c){
-		if(c.collider.name == "NecroFT(Clone)"){
-			Debug.Log ("jump");
-			player.rigidbody2D.AddForce(new Vector2(0, power));
+		if(c.collider.gameObject.tag == "Player"){
+			c.gameObject.rigidbody2D.AddForce(new Vector2(0, power));
 		}
 	}
 }
