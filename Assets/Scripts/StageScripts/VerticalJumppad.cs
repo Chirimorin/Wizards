4f47f321d6fe374a290fd77fc8855b84e0aa73da
Thomas Voogt <chirimorin@gmail.com>
2014-07-11 13:47:25 +0200

    Velocity reset on jump

--- Assets/Scripts/StageScripts/VerticalJumppad.cs
+++ Assets/Scripts/StageScripts/VerticalJumppad.cs
@@ -16,6 +16,7 @@ public class VerticalJumppad : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D c){
 		if(c.collider.gameObject.tag == "Player"){
+			c.gameObject.rigidbody2D.velocity = new Vector2(c.gameObject.rigidbody2D.velocity.x, 0);
 			c.gameObject.rigidbody2D.AddForce(new Vector2(0, power));
 		}
 	}
