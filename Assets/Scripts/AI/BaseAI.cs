4601c55f2d767fba3670107bac637b0f78101cc6
Jorentjuh <jorentjuh@hotmail.com>
2014-07-21 12:04:48 +0200

    baseAI start

--- Assets/Scripts/AI/BaseAI.cs
+++ Assets/Scripts/AI/BaseAI.cs
@@ -3,13 +3,30 @@ using System.Collections;
 
 public class BaseAI : MonoBehaviour {
 
+	private float stunDuration;
+	protected bool stunned;
+	private float stunTimer;
+
 	// Use this for initialization
-	void Start () {
-	
+	protected void Start () {
+		stunned = false;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-	
+	protected void Update () {
+		if(stunned){
+			stunTimer += Time.deltaTime;
+			if(stunTimer > stunDuration){
+				stunTimer = 0;
+				stunned = false;
+			}
+		}
+
 	}
+
+	public void Stun(float stunDuration){
+		this.stunDuration = stunDuration;
+		stunned = true;
+	}
+
 }
